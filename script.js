64f827804f976b6f4bde80158331dd9ea1a279cc
MacOS <macos@blessing.home>
2022-11-17 21:42:29 +0000

    feat: adding logic

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-document.getElementById("count").innerText = 5
+// document.getElementById("count").innerText = 5
+
+let count = 0
+function increment (){
+    count +=1
+    document.getElementById("count").innerText = count
+    console.log('button is clicked')
+}
+
+let saveEl =document.getElementById('save-el')
+
+function save () {
+let countDash = count + "-"
+saveEl.innerText+=countDash
+document.getElementById("count").innerText = 0
+count=0
+}
